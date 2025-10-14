35bae80e6f9db5f1394bb50d608874298958a512
ethan <ethanbernard29@gmail.com>
2025-10-14 16:06:26 +0200

    Ajout de la vue vueErreur pour la gestion des erreurs

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<?php require 'Modele.php';
-$billets = getBillets();
-// Affichage
-$contenu = 'vueAccueil.php';
-require 'gabarit.php';
+<?php
+require 'Modele.php';
+try {
+    $billets = getBillets();
+    // Affichage
+    $contenu = 'vueAccueil.php';
+    require 'gabarit.php';
+} catch (Exception $e) {
+    $msgErreur = $e ->getMessage();
+    $contenu = 'vueErreur.php';
+    require 'gabarit.php';
+}
